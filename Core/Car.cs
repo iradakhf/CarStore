714bb2b418d10bc3c4f7b1bc040b240494082d18
iradakhf <iradakhf@code.edu.az>
2022-08-15 16:38:48 +0400

    add

--- Core/Car.cs
+++ Core/Car.cs
@@ -1,10 +1,14 @@
-﻿namespace Core
+﻿using Core.Abstracts;
+
+namespace Core
 {
-    public class Car
+    public class Car : IEntity
     {
         public string Name { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
+        public string Color { get; set; }
         public CarStore CarStore { get; set; }
+        public int Id { get ; set ; }
     }
 }
