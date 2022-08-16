bdb0c2f729c0ca78d6f601e857340852064ac085
iradakhf <iradakhf@code.edu.az>
2022-08-16 07:05:21 +0400

    feat : owner create method done

---
+++ CarStore/Controllers/CarController.cs
@@ -0,0 +1,10 @@
+﻿
+using DataAccess.Implementations;
+
+namespace Manage.Controllers
+{
+    public class CarController
+    {
+        public CarRepository _carRepository;
+    }
+}
---
+++ CarStore/Controllers/OwnerController.cs
@@ -0,0 +1,58 @@
+﻿using Core;
+using Core.Helpers;
+using DataAccess.Implementations;
+
+namespace Manage.Controllers
+{
+    public class OwnerController
+    {
+        public OwnerRepository _ownerRepository;
+        public OwnerController()
+        {
+            _ownerRepository = new OwnerRepository();
+        }
+        public void Create()
+        {
+             ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkCyan, "Please enter the name");
+            string name = Console.ReadLine();
+            ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkCyan, "Please enter the surname");
+            string surname = Console.ReadLine();
+            Age: ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkCyan, "Please enter the age ");
+            string age = Console.ReadLine();
+            byte Age;
+            bool result = byte.TryParse(age, out Age);
+            if (result)
+            {
+                Owner owner = new Owner
+                {
+                    Age = Age,
+                    Name=name,
+                    Surname=surname
+                    
+                };
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkRed, "Please enter the age in the correct format ");
+                goto Age;
+            }
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+        public void Get()
+        {
+
+        }
+        public void GetAll()
+        {
+
+        }
+    }
+}
